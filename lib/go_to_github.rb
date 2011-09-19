ed01d14449f49a1c96d12de4c028a02d5eed8978
Ryan Taylor Long <ryan@rtlong.com>
2011-09-19 02:40:23 -0500

    Provide a default remote if one is not yet set up in git for this branch

--- lib/go_to_github.rb
+++ lib/go_to_github.rb
@@ -1,22 +1,22 @@
 module Redcar
-  
+
   class GoToGithub
-    
+
     def self.keymaps
       osx = Keymap.build("main", :osx) do
         link "Cmd+Shift+G", GoToGithub::ShowLineInGithubBlob
         link "Cmd+Shift+B", GoToGithub::ShowLineInGithubBlame
       end
-      
+
       linwin = Keymap.build("main", [:linux, :windows]) do
         link "Ctrl+Shift+G", GoToGithub::ShowLineInGithubBlob
         link "Ctrl+Shift+B", GoToGithub::ShowLineInGithubBlame
       end
-      
+
       [linwin, osx]
     end
-    
-    def self.menus      
+
+    def self.menus
       Menu::Builder.build do
         sub_menu "Plugins" do
           sub_menu "Go to GitHub" do
@@ -28,48 +28,51 @@ module Redcar
       end
     end
 
-    
+
     class ShowLineInGithubBlob < Redcar::Command
-      def execute                    
+      def execute
         GenerateGithubLink.new(win)
       end
     end
-    
+
     class ShowLineInGithubBlame < Redcar::Command
       def execute
         GenerateGithubLink.new(win, 'blame')
       end
     end
-        
+
     class EditGoToGithub < Redcar::Command
-      def execute        
-        Project::Manager.open_project_for_path(File.join(Redcar.user_dir, "plugins", "go-to-github"))                
-        tab  = Redcar.app.focussed_window.new_tab(Redcar::EditTab)                
+      def execute
+        Project::Manager.open_project_for_path(File.join(Redcar.user_dir, "plugins", "go-to-github"))
+        tab  = Redcar.app.focussed_window.new_tab(Redcar::EditTab)
         mirror = Project::FileMirror.new(File.join(Redcar.user_dir, "plugins", "go-to-github", "lib", "go_to_github.rb"))
-        tab.edit_view.document.mirror = mirror        
+        tab.edit_view.document.mirror = mirror
         tab.edit_view.reset_undo
         tab.focus
       end
     end
-    
+
     private
-    
+
     class GenerateGithubLink
-      
-      def initialize(win, type = 'blob')        
+
+      def initialize(win, type = 'blob')
         begin
           document = win.focussed_notebook.focussed_tab.document
           current_line = document.cursor_line + 1
           path = Project::Manager.focussed_project.path
           file = document.path[path.size,document.path.size]
           branch = `cd #{path} && git branch --no-color 2> /dev/null`.match(/\*\ (.*)\n/)[1]
-          remote = `cd #{path} && git config --get branch.#{branch}.remote`.chomp
-          project_name = `cd #{path} && git config --get remote.#{remote}.url`.match(/github.com[:|\/](.*)\.git\n/)[1]                                        
-          
-          raise unless branch && remote && project_name
-          
           url = "https://github.com/" << project_name << "/" << type << "/" << branch << file << "#L" << current_line.to_s                    
     
+          remote = `cd #{path} && git config --get branch.#{branch}.remote`
+          remote = 'origin' if remote.empty?
+          project_name = `cd #{path} && git config --get remote.#{remote}.url`.match(/github.com[:|\/](.*)\.git\n/)[1]
+
+          raise "Unable to determine current branch" unless branch
+          raise "Unable to determine correct GitHub remote" unless remote
+          raise "Unable to determine GitHub user/project names" unless project_name
+
           Thread.new do
             case Redcar.platform
             when :osx
@@ -79,14 +82,14 @@ module Redcar
             when :windows
               system("start " << url)
             end
-          end 
-          
-        rescue
-          Application::Dialog.message_box("This project is not Github repository")
+          end
+
+        rescue e
+          Application::Dialog.message_box("This project is not Github repository:\n\n#{e.message}")
         end
-      end      
+      end
     end
-    
+
   end
-  
+
 end
